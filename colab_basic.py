13bcba5781040752fc65bb037c7772d08d488944
Iporã Possantti <61351163+ipo-exe@users.noreply.github.com>
2022-07-20 14:42:14 -0300

    Update colab_basic.py

--- colab_basic.py
+++ colab_basic.py
@@ -1,3 +1,45 @@
+'''
+
+Google Earth Engine Script for the colab API (Python)
+
+>>> generic routine for downloading imagery 
+
+Copyright (C) 2022 Iporã Brito Possantti
+
+************ GNU GENERAL PUBLIC LICENSE ************
+
+https://www.gnu.org/licenses/gpl-3.0.en.html
+Permissions:
+ - Commercial use
+ - Distribution
+ - Modification
+ - Patent use
+ - Private use
+Conditions:
+ - Disclose source
+ - License and copyright notice
+ - Same license
+ - State changes
+Limitations:
+ - Liability
+ - Warranty
+ 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  
+
+If not, see <https://www.gnu.org/licenses/>.
+'''
+
 # [1] -- install ee
 !pip install earthengine-api
 
